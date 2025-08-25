8426e15a4694b953120fe75e6b1a7084ad0884af
Leon Davis <leonskij@proton.me>
2025-08-25 06:45:58 +0100

    format

--- src/lib.rs
+++ src/lib.rs
@@ -239,141 +239,143 @@ impl Order {
   }
 }
 
-pub static STATIONS: LazyLock<Vec<Station>> = LazyLock::new(|| vec![
-  Station::new(
-    "CME",
-    "Coal Mine East",
-    HashMap::from([
-      ("A", vec![3]),
-      ("B", vec![1, 2, 3, 5]),
-      ("C", vec![1, 3]),
-    ]),
-  ),
-  Station::new(
-    "CMS",
-    "Coal Mine South",
-    HashMap::from([("A", vec![1, 2]), ("B", vec![2, 3, 4, 5, 6, 7, 8])]),
-  ),
-  Station::new(
-    "CP",
-    "Coal Power Plant",
-    HashMap::from([("A", vec![1, 2, 3, 4, 5, 6]), ("B", vec![1, 2, 3, 5])]),
-  ),
-  Station::new(
-    "CS",
-    "City South",
-    HashMap::from([
-      ("A", vec![3]),
-      ("B", vec![1, 3, 4]),
-      ("C", vec![1, 3, 4]),
-    ]),
-  ),
-  Station::new(
-    "CW",
-    "City West",
-    HashMap::from([("A", vec![1, 3]), ("C", vec![2, 3, 4, 5, 6])]),
-  ),
-  Station::new(
-    "FF",
-    "Food Factory and Town",
-    HashMap::from([
-      ("A", vec![1]),
-      ("C", vec![1, 2, 3, 4, 5, 6, 7, 8, 9]),
-      ("D", vec![1, 2, 3, 4]),
-    ]),
-  ),
-  Station::new(
-    "FM",
-    "Farm",
-    HashMap::from([("A", vec![1, 2, 3]), ("B", vec![1, 2, 3, 5, 6])]),
-  ),
-  Station::new(
-    "FRC",
-    "Forest Central",
-    HashMap::from([("B", vec![1, 2, 4]), ("C", vec![1, 2, 4])]),
-  ),
-  Station::new(
-    "FRS",
-    "Forest South",
-    HashMap::from([("A", vec![1, 2, 3, 5, 6, 7])]),
-  ),
-  Station::new(
-    "GF",
-    "Goods Factory and Town",
-    HashMap::from([
-      ("A", vec![2, 3]),
-      ("B", vec![1, 2, 3]),
-      ("D", vec![1, 2, 3, 5, 6, 7]),
-    ]),
-  ),
-  Station::new(
-    "HB",
-    "Harbor and Town",
-    HashMap::from([
-      ("B", vec![1, 3, 4, 5, 6, 7, 8]),
-      ("C", vec![1, 2, 3]),
-      ("D", vec![1, 2, 3, 4, 5, 6, 7]),
-      ("E", vec![1, 2, 3, 4, 5, 7, 8, 9, 10, 11]),
-      ("G", vec![1, 2, 3, 5, 6, 7]),
-    ]),
-  ),
-  Station::new(
-    "IME",
-    "Iron Ore Mine East",
-    HashMap::from([
-      ("A", vec![1]),
-      ("B", vec![1, 2, 4]),
-      ("C", vec![1, 3, 4]),
-    ]),
-  ),
-  Station::new(
-    "IMW",
-    "Iron Ore Mine West",
-    HashMap::from([("A", vec![1, 2, 3, 4, 6, 7, 8])]),
-  ),
-  Station::new(
-    "MB",
-    "Military Base",
-    HashMap::from([("A", vec![1, 2]), ("B", vec![2, 3, 4, 5, 6])]),
-  ),
-  Station::new(
-    "MF",
-    "Machine Factory and Town",
-    HashMap::from([
-      ("B", vec![1, 2, 4, 5, 6]),
-      ("C", vec![1, 2, 3, 4]),
-      ("E", vec![1, 2, 3, 4]),
-    ]),
-  ),
-  Station::new(
-    "OR",
-    "Oil Refinery",
-    HashMap::from([
-      ("A", vec![1, 2, 3, 4, 5, 6]),
-      ("B", vec![3, 4, 5, 6, 7]),
-    ]),
-  ),
-  Station::new(
-    "OWC",
-    "Oil Well Central",
-    HashMap::from([("A", vec![1, 2, 3]), ("B", vec![1, 3, 4, 5, 6])]),
-  ),
-  Station::new(
-    "OWN",
-    "Oil Well North",
-    HashMap::from([("B", vec![2, 3, 4, 5, 6]), ("C", vec![1, 3])]),
-  ),
-  Station::new(
-    "SM",
-    "Steel Mill",
-    HashMap::from([
-      ("A", vec![3, 4, 5, 6, 7]),
-      ("B", vec![1, 2, 3, 4, 6, 7, 8]),
-    ]),
-  ),
-  Station::new(
-    "SW",
-    "Saw Mill",
-    HashMap::from([("B", vec![1, 3, 4]), ("C", vec![1, 3, 4])]),
-  ),
-]);
+pub static STATIONS: LazyLock<Vec<Station>> = LazyLock::new(|| {
+  vec![
+    Station::new(
+      "CME",
+      "Coal Mine East",
+      HashMap::from([
+        ("A", vec![3]),
+        ("B", vec![1, 2, 3, 5]),
+        ("C", vec![1, 3]),
+      ]),
+    ),
+    Station::new(
+      "CMS",
+      "Coal Mine South",
+      HashMap::from([("A", vec![1, 2]), ("B", vec![2, 3, 4, 5, 6, 7, 8])]),
+    ),
+    Station::new(
+      "CP",
+      "Coal Power Plant",
+      HashMap::from([("A", vec![1, 2, 3, 4, 5, 6]), ("B", vec![1, 2, 3, 5])]),
+    ),
+    Station::new(
+      "CS",
+      "City South",
+      HashMap::from([
+        ("A", vec![3]),
+        ("B", vec![1, 3, 4]),
+        ("C", vec![1, 3, 4]),
+      ]),
+    ),
+    Station::new(
+      "CW",
+      "City West",
+      HashMap::from([("A", vec![1, 3]), ("C", vec![2, 3, 4, 5, 6])]),
+    ),
+    Station::new(
+      "FF",
+      "Food Factory and Town",
+      HashMap::from([
+        ("A", vec![1]),
+        ("C", vec![1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        ("D", vec![1, 2, 3, 4]),
+      ]),
+    ),
+    Station::new(
+      "FM",
+      "Farm",
+      HashMap::from([("A", vec![1, 2, 3]), ("B", vec![1, 2, 3, 5, 6])]),
+    ),
+    Station::new(
+      "FRC",
+      "Forest Central",
+      HashMap::from([("B", vec![1, 2, 4]), ("C", vec![1, 2, 4])]),
+    ),
+    Station::new(
+      "FRS",
+      "Forest South",
+      HashMap::from([("A", vec![1, 2, 3, 5, 6, 7])]),
+    ),
+    Station::new(
+      "GF",
+      "Goods Factory and Town",
+      HashMap::from([
+        ("A", vec![2, 3]),
+        ("B", vec![1, 2, 3]),
+        ("D", vec![1, 2, 3, 5, 6, 7]),
+      ]),
+    ),
+    Station::new(
+      "HB",
+      "Harbor and Town",
+      HashMap::from([
+        ("B", vec![1, 3, 4, 5, 6, 7, 8]),
+        ("C", vec![1, 2, 3]),
+        ("D", vec![1, 2, 3, 4, 5, 6, 7]),
+        ("E", vec![1, 2, 3, 4, 5, 7, 8, 9, 10, 11]),
+        ("G", vec![1, 2, 3, 5, 6, 7]),
+      ]),
+    ),
+    Station::new(
+      "IME",
+      "Iron Ore Mine East",
+      HashMap::from([
+        ("A", vec![1]),
+        ("B", vec![1, 2, 4]),
+        ("C", vec![1, 3, 4]),
+      ]),
+    ),
+    Station::new(
+      "IMW",
+      "Iron Ore Mine West",
+      HashMap::from([("A", vec![1, 2, 3, 4, 6, 7, 8])]),
+    ),
+    Station::new(
+      "MB",
+      "Military Base",
+      HashMap::from([("A", vec![1, 2]), ("B", vec![2, 3, 4, 5, 6])]),
+    ),
+    Station::new(
+      "MF",
+      "Machine Factory and Town",
+      HashMap::from([
+        ("B", vec![1, 2, 4, 5, 6]),
+        ("C", vec![1, 2, 3, 4]),
+        ("E", vec![1, 2, 3, 4]),
+      ]),
+    ),
+    Station::new(
+      "OR",
+      "Oil Refinery",
+      HashMap::from([
+        ("A", vec![1, 2, 3, 4, 5, 6]),
+        ("B", vec![3, 4, 5, 6, 7]),
+      ]),
+    ),
+    Station::new(
+      "OWC",
+      "Oil Well Central",
+      HashMap::from([("A", vec![1, 2, 3]), ("B", vec![1, 3, 4, 5, 6])]),
+    ),
+    Station::new(
+      "OWN",
+      "Oil Well North",
+      HashMap::from([("B", vec![2, 3, 4, 5, 6]), ("C", vec![1, 3])]),
+    ),
+    Station::new(
+      "SM",
+      "Steel Mill",
+      HashMap::from([
+        ("A", vec![3, 4, 5, 6, 7]),
+        ("B", vec![1, 2, 3, 4, 6, 7, 8]),
+      ]),
+    ),
+    Station::new(
+      "SW",
+      "Saw Mill",
+      HashMap::from([("B", vec![1, 3, 4]), ("C", vec![1, 3, 4])]),
+    ),
+  ]
+});
